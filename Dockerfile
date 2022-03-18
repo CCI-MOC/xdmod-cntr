0fd40ce52f0d8bc8f9ac3545042f3ba3146afb4d
Robert Baron <robbaron@bu.edu>
2022-03-18 14:01:41 -0400

    this updates the draft pull request

--- Dockerfile
+++ Dockerfile
@@ -9,29 +9,40 @@ WORKDIR /root
 
 # This installs a barebones 9.5 instance of XDmod
 RUN mkdir -p /root/rpmbuild/RPMS/noarch \
- && mkdir /tmp/supremm \
- && yum -y install openssh-server openssh-clients pcp pcp-manager pcp-conf pcp-libs python-pcp perl-PCP-PMDA pcp-system-tools pcp-pmda-gpfs pcp-pmda-lustre pcp-pmda-infiniband pcp-pmda-mic pcp-pmda-nvidia-gpu \
-              pcp-pmda-nfsclient pcp-pmda-perfevent pcp-pmda-json \
- && 
+    && mkdir /tmp/supremm \
+    && yum -y install openssh-server openssh-clients pcp pcp-manager pcp-conf pcp-libs python-pcp perl-PCP-PMDA pcp-system-tools pcp-pmda-gpfs pcp-pmda-lustre pcp-pmda-infiniband pcp-pmda-mic pcp-pmda-nvidia-gpu \
+    pcp-pmda-nfsclient pcp-pmda-perfevent pcp-pmda-json python3 \
+    && pip3 install --upgrade pip \
+    && pip3 install keystoneauth python-keystoneclient python-novaclient python-cinderclient cryptography python-openstacksdk mysql-connector-python
 
 # RPM install of XDMod
 COPY ./httpd.conf /etc/httpd/conf/httpd.conf
 COPY ./portal_settings.ini /etc/xdmod/portal_settings.ini
+RUN chmod 777 /etc/xdmod/portal_settings.ini
+COPY ./etc-xdmod-resources.json /etc/xdmod/resources.json
+COPY ./etc-xdmod-resource_specs.json /etc/xdmod/resource_specs.json
+COPY ./etc-xdmod-organization.json /etc/xdmod/organization.json
+COPY ./openstack.2018-04-17T00-00-00_2018-04-30T23-59-59.json /root/test/openstack/2018-04-17T00-00-00_2018-04-30T23-59-59.json
+
 # am copying the xdmod-httpd conf file to ssl.conf as both use *:443
 # and this is easier than just deleting the ssl.conf
 COPY ./xdmod-httpd.conf /etc/httpd/conf.d/ssl.conf
 RUN cd /root/rpmbuild/RPMS/noarch \
- && wget -nv https://github.com/ubccr/xdmod/releases/download/v9.5.0/xdmod-9.5.0-1.0.el7.noarch.rpm \
- && yum -y install xdmod-9.5.0-1.0.el7.noarch.rpm
+    && wget -nv https://github.com/ubccr/xdmod/releases/download/v9.5.0/xdmod-9.5.0-1.0.el7.noarch.rpm \
+    && yum -y install xdmod-9.5.0-1.0.el7.noarch.rpm
 
-# This is needed as the xdmod-setup script is a bit broken
-COPY ./xdmod_mariadb.sql /root/xdmod_mariadb.sql
-RUN mysql -host mariadb --protocol=tcp -u root -ppass < /root/xdmod_mariadb.sql
+COPY ./hypervisor_facts.py /usr/bin/xdmod-openstack-hypervisor
+COPY ./moc_openstack_api_reporting.py /usr/bin/xdmod-openstack-reporting
+RUN chmod 777 /usr/bin/xdmod-openstack-hypervisor \
+    && sed -i -e 's/\r$//' /usr/bin/xdmod-openstack-hypervisor \
+    && chmod 777 /usr/bin/xdmod-openstack-reporting \
+    && sed -i -e 's/\r$//' /usr/bin/xdmod-openstack-reporting
 
 # This is done to work-a-round xdmod-setup and supremm-setup being interactive
-RUN pip install pexpect
+# RUN pip install pexpect
 # pexpect-xdmod-setup runs xdmod-setup non-interactively
-COPY ./pexpect-xdmod-setup.py /root/pexpect-xdmod-setup.py
+# haven't written this yet
+# COPY ./pexpect-xdmod-setup.py /root/pexpect-xdmod-setup.py
 
 
 # This is a work-a-round as the xdmod-setup script need to be fixed
@@ -41,22 +52,13 @@ COPY ./pexpect-xdmod-setup.py /root/pexpect-xdmod-setup.py
 
 
 # RPM install of supremm
-COPY ./supremmconfig.json /etc/xdmod/supremmconfig.json
-RUN cd /root/rpmbuild/RPMS/noarch \
- && wget -nv https://github.com/ubccr/xdmod-supremm/releases/download/v9.5.0/xdmod-supremm-9.5.0-1.0.el7.noarch.rpm \
- yum -y install xdmod-supremm-9.5.0-1.0.el7.noarch.rpm 
-
+# COPY ./supremmconfig.json /etc/xdmod/supremmconfig.json
+# RUN cd /root/rpmbuild/RPMS/noarch \
+#  && wget -nv https://github.com/ubccr/xdmod-supremm/releases/download/v9.5.0/xdmod-supremm-9.5.0-1.0.el7.noarch.rpm \
+#  yum -y install xdmod-supremm-9.5.0-1.0.el7.noarch.rpm 
 # need to figure out how to install supremm-setup utility
 
 
 
-# From the original Dockerfile
-#RUN mkdir -p /root/rpmbuild/RPMS/noarch \
-#    && wget -nv -P /root/rpmbuild/RPMS/noarch https://github.com/ubccr/xdmod/releases/download/v$REL/xdmod-$REL-$BUILD.el7.noarch.rpm \
-#    && git clone --single-branch https://github.com/ubccr/xdmod/ --branch $BRANCH /root/xdmod \
-#    && /root/xdmod/tests/ci/bootstrap.sh \
-#    && ~/bin/services stop \
-#    && yum clean all \
-#    && rm -rf /var/cache/yum /root/xdmod /root/rpmbuild
 
 WORKDIR /
--- hypervisor_facts.py
+++ hypervisor_facts.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+""" This just returns the hypervisor facts """
 
-import os
 import json
 import argparse
 import logging
 import datetime
-import calendar
-import requests
 import glob
 import openstack
 
+
 def deep_compare(obj):
+    """compares each item in a object or a dictionary"""
     if isinstance(obj, dict):
         return sorted((k, deep_compare(v)) for k, v in obj.items())
     if isinstance(obj, list):
@@ -18,105 +18,111 @@ def deep_compare(obj):
     else:
         return obj
 
-def doParseArgs(config):
-    """Parse args and return a config dict"""
 
-    parser = argparse.ArgumentParser(description='Generate accounting records for OpenStack instances', epilog='-D and -A are mutually exclusive')
+def do_parse_args(config):
+    """Parse args and return a config dict"""
+    parser = argparse.ArgumentParser(description="Generate accounting records for OpenStack instances", epilog="-D and -A are mutually exclusive")
     parser.add_argument("-v", "--verbose", help="output debugging information", action="store_true")
     parser.add_argument("-C", "--config-file", help="Configuration file")
     parser.add_argument("-o", "--outdir", help="Output directory")
+    parser.add_argument("--cloud", help="cloud name (in .config/openstack/cloud.json)")
 
     args = parser.parse_args()
 
-
-    config['loglevel']=logging.CRITICAL
-    config['config_file'] = '/path/to/config.conf'
+    config["loglevel"] = logging.CRITICAL
+    config["config_file"] = "/path/to/config.conf"
 
     if args.config_file:
-        config['config_file'] = args.config_file
+        config["config_file"] = args.config_file
 
-    config['outdir'] = '.'
+    config["outdir"] = "."
     if args.outdir:
-        config['outdir'] = args.outdir
+        config["outdir"] = args.outdir
 
     if args.verbose:
-        config['loglevel']=logging.INFO
+        config["loglevel"] = logging.INFO
+
+    if args.cloud:
+        config["cloud"] = args.cloud
 
     return config
 
-def doReadConfig(config):
+
+def do_read_config(config):
+    """reads from the config file if present"""
     try:
-        f = open(config['config_file'], 'r')
+        f = open(config["config_file"], "r")
     except IOError:
         return
     else:
         newconfig = json.load(f)
         config.update(newconfig)
 
-def getData(config):
- 
-    # TODO: add cloud parameter to config
-    openstack_connection=openstack.connect(cloud='admin-kaizen')
-    
-    hvs=[]
-    hv_status={}
+
+def get_data(config):
+    """Gets the data from OpenStack using the openstacksdk"""
+    openstack_connection = openstack.connect(cloud=config["cloud"])
+
+    hvs = []
+    hv_status = {}
 
     for nc in openstack_connection.compute.hypervisors(details=True):
-        hv={}
-        hv['id']=nc.id
-        hv['hypervisor_hostname']=nc.name
-        hv['vcpus']=nc.vcpus
-        hv['memory_mb']=nc.memory_size
+        hv = {}
+        hv["id"] = nc.id
+        hv["hypervisor_hostname"] = nc.name
+        hv["vcpus"] = nc.vcpus
+        hv["memory_mb"] = nc.memory_size
         hvs.append(hv)
 
-    hv_status['hypervisors']=hvs
+    hv_status["hypervisors"] = hvs
     ts = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-    hv_status['ts']=ts
+    hv_status["ts"] = ts
     return hv_status
 
-def getLatestFacts(config):
-    file_list = glob.glob(config['outdir'] + "//hypervisor_facts_*.json")
+
+def get_latest_facts(config):
+    """gets the previously recorded hypervisor facts"""
+    file_list = glob.glob(config["outdir"] + "//hypervisor_facts_*.json")
 
     if len(file_list) == 0:
         return {"hypervisors": []}
 
-    newest = max(file_list, key=lambda d: datetime.datetime.strptime(d, "{}/hypervisor_facts_%Y-%m-%dT%H:%M:%S.json".format(config['outdir'])))
+    newest = max(file_list, key=lambda d: datetime.datetime.strptime(d, "{}/hypervisor_facts_%Y-%m-%dT%H:%M:%S.json".format(config["outdir"])))
 
-    f=open(newest, 'r')
-    latest_facts=json.load(f)
+    f = open(newest, "r")
+    latest_facts = json.load(f)
 
     return latest_facts
 
-def isNewData(config,data):
-    latest_facts = getLatestFacts(config)
-    if deep_compare(latest_facts['hypervisors']) == deep_compare(data['hypervisors']):
+
+def is_new_data(config, data):
+    """Checks to see if the hypervisor facts have changed"""
+    latest_facts = get_latest_facts(config)
+    if deep_compare(latest_facts["hypervisors"]) == deep_compare(data["hypervisors"]):
         logging.info("No new facts found")
         return False
     else:
         logging.info("New facts found")
         return True
 
-def main ():
 
-    config={}
+def main():
+    """the main function"""
+    config = {}
 
-    doParseArgs(config)
-    doReadConfig(config)
+    do_parse_args(config)
+    do_read_config(config)
 
-    logging.basicConfig(
-                format='%(asctime)s [%(levelname)s] %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S',
-                level=config['loglevel']
-                )
+    logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=config["loglevel"])
 
+    data = get_data(config)
 
-    data = getData(config)
-
-    if isNewData(config, data):
-        nowtime = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        json_out = "{}/hypervisor_facts_{}.json".format(config['outdir'], nowtime)
-        with open(json_out, 'w') as outfile:
+    if is_new_data(config, data):
+        nowtime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+        json_out = "{}/hypervisor_facts_{}.json".format(config["outdir"], nowtime)
+        with open(json_out, "w", encoding="utf-8") as outfile:
             json.dump(data, outfile, indent=2, sort_keys=True)
+
 
 if __name__ == "__main__":
     main()
