52ab39c458ad1ed60db5e41506bf85df1453e2b0
Lars Kellogg-Stedman <lars@oddbit.com>
2022-08-01 10:52:31 -0400

    Use database for storing/fetching config files in lieu of RWX storage (#51)
    * Store config files in the database
    
    Rather than relying on a shared volume, store generated configuration
    files in the database.
    
    * Allow xdmod to write to /etc/httpd and /etc/xmod
    
    The xmod user requires write access to these directories in order to
    update the xdmod configuration and the Apache configuration at
    runtime.
    
    * Add GetConfigFiles.py
    
    GetConfigFiles.py fetches configuration files from the
    database and writes them to the local filesystem.
    
    * Add xdmod-run-ingestor script
    
    xdmod-run-ingestor is a script to fetch configuration files from the
    database and then run the ingestor.
    
    * Remove carriage returns from xdmod-get-config-files
    
    * Prefer numeric file modes
    
    The 'X' doesn't not seem to set the execute bit on the script, and I
    am more use to seeing 774 as opposed to the symbolic modes.
    
    * Upload tarball of /etc/xdmod directory to database
    
    Rather than uploading/downloading individual files, tar up /etc/xdmod
    and upload the resulting tarball to the database.
    
    * Sleep before calling ingestor
    
    We have been experiencing some database connectivity problems. This
    commit attempts to reduce the rate of connections to the database by
    adding a sleep before calling the ingestor.
    
    * Simplify SQL error handling in GetConfigFiles.py
    
    Fail with default traceback rather than trying to trap exceptions and
    emit a custom error message.
    
    * Simplify check for xdmod_init.json
    
    Having both the os.path.isfile guard and the exception handling was
    redundant. We can handle a missing file via an exception.
    
    * Remove unused code from GetConfigFiles.py
    
    * Add some docstrings to GetConfigFiles.py
    
    * Shorten error message when xdmod-init.json is missing
    
    * Use context manager form of subprocess.run
    
    Using subprocess.run as a context manager ensures that we will
    wait() on the child process.
    
    * Simplify exception handling in xdmod-init.py
    
    Fail with default traceback rather than trying to trap exceptions and
    emit a custom error message.
    
    * Remove unused code in xdmod_init.py
    
    * Disable even more pylint warnings
    
    * Clean up tarball generation
    
    Run tar in verbose mode and clean up artifacts after uploading
    the tarball to the database.
    
    Co-authored-by: rob-baron <robbaron@bu.edu>

---
+++ GetConfigFiles.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" This pulls the config files from the database and copies them in to /etc/xdmod
+
+    Will be unnecessary if RWX volumes become available
+"""
+# pylint: disable=line-too-long invalid-name
+import os
+import subprocess
+import json
+import mysql.connector
+
+
+def exec_fetchall(cursor, sql_stmt, params):
+    """executes the sql statmement and fetches all in a list"""
+    cursor.execute(sql_stmt, params)
+    result = cursor.fetchall()
+    return result
+
+
+def connect_to_db(database):
+    """This just connects to the database and returns the connection"""
+    host = database["host"]
+    admin_acct = "root"
+    admin_pass = database["admin_password"]
+    # it is ok if the file doesn't as the clouds.yaml is possibly empty or manually updated
+    cnx = mysql.connector.connect(host=host, user=admin_acct, password=admin_pass)
+    return cnx
+
+
+def write_file_from_db(cursor, script):
+    """As a work-a-round for RWM, share config files though the database"""
+    print(f"write {script} file_share_db")
+    data = exec_fetchall(cursor, "select file_name, file_data from file_share_db.file where script=%s", (script,))
+
+    if data and isinstance(data, list):
+        rec = data[0]
+        file = rec[0]
+        directory = os.path.dirname(file)
+        if not os.path.isdir(directory):
+            print(f"Creating Direcotry {directory}")
+            os.makedirs(directory)
+        with open(file, "wb+") as fp:
+            fp.write(rec[1])
+
+
+def main():
+    """the main function"""
+    config = {}
+
+    try:
+        with open("/etc/xdmod/xdmod_init.json", "r", encoding="utf-8") as file:
+            config = json.load(file)["database"]
+    except IOError:
+        print("Ensure the xdmod-init.json file is in /etc/xdmod/xdmod_init.json")
+
+    cnx = connect_to_db(config)
+    cursor = cnx.cursor()
+
+    write_file_from_db(cursor, "etc-xdmod")
+    with subprocess.Popen(["/usr/bin/base64", "-d", "/etc/xdmod/etc_xdmod.b64"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as base64:
+        b64_out, b64_err = base64.communicate()
+        print(f"errors: {b64_err}")
+        with open("/etc/xdmod/etc_xdmod.tgz", "wb") as fptr:
+            fptr.write(b64_out)
+        os.system("/usr/bin/tar -xzf /etc/xdmod/etc_xdmod.tgz")
+
+    cnx.commit()
+    cnx.close()
+
+
+if __name__ == "__main__":
+    main()
