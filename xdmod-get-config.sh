437eee80cfb41ca32e17925dcf5a26872454d453
rob-baron <robbaron@bu.edu>
2023-01-20 17:57:50 -0500

    Rework of the Ingestor job (#121)
    * Rework of the Ingestor job
    
    Split off the downloading of the config from the database into
    an init container from the running of xdmod-ingestor in the
    main container.
    
    * Stop shortening get-config
    
    xdmod-get-config is a better name than xdmod-get-conf.
    
    * Use the correct image
    
    I don't see a reason why I should prepent a moc for images at this
    time as they are potentially useful outside of the moc.
    
    * corrected the script
    
    One of the annoying differences between centos and the python3 container
    is the location of the shell command.  This fixes it (pthyon 3: #!/bin/bash,
    centos 7: #!/usr/bin/sh)
    
    * Delete an unused file
    
    Am no longer calling xdmod-ingestor from the shell script
    as the shell script has been replaced by xdmod-get-configs.sh
    which is called in a init container.
    
    * Calling the Correct Command
    
    On the python container all execuatable files are loaded into the
    /app directory as opposed to on the centos based xdmod container where
    they are installed in /usr/bin.

---
+++ xdmod-get-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /mnt/xdmod_init/xdmod_init.json /etc/xdmod/xdmod_init.json
+/app/xdmod-get-config-files
+cp /etc/xdmod/clouds.yaml /etc/openstack/clouds.yaml 
--- xdmod-run-ingestor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-cp /mnt/xdmod_init/xdmod_init.json /etc/xdmod/xdmod_init.json
-/usr/bin/xdmod-get-config-files 
-/usr/bin/sleep 30
-/usr/bin/xdmod-ingestor
