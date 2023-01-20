fce0819cce9699cf4b07adebe3a803b00a206030
rob-baron <robbaron@bu.edu>
2023-01-20 14:58:37 -0500

    Calling the Correct Command
    On the python container all execuatable files are loaded into the
    /app directory as opposed to on the centos based xdmod container where
    they are installed in /usr/bin.

--- xdmod-get-config.sh
+++ xdmod-get-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp /mnt/xdmod_init/xdmod_init.json /etc/xdmod/xdmod_init.json
-/usr/bin/xdmod-get-config-files
+/app/xdmod-get-config-files
 cp /etc/xdmod/clouds.yaml /etc/openstack/clouds.yaml 
