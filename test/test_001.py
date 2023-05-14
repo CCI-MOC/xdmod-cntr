81af52517164fd90ee88e18086f4ed42b963d11b
rob-baron <robbaron@bu.edu>
2023-05-14 17:44:20 -0400

    Added test_002 to start testing for the second run
    Change the data that was used in test_001 a bit to remove
    a PI and projects, and want to see that pi and those projects
    removed from the output.

--- test/test_001.py
+++ test/test_001.py
@@ -10,7 +10,7 @@ dataset = {
     "0": {
         "coldfront_data": [],
         "keycloak_data": [],
-        "hierarchy.csv": ['"1","unknown",\n', '"2","unknown","1"\n'],
+        "hierarchy.csv": [],
         "group.csv": [],
         "pi2project.csv": [],
         "names.csv": [],
@@ -125,13 +125,11 @@ dataset = {
             },
         ],
         "hierarchy.csv": [
-            '"1","unknown",\n',
             '"3","Some University",\n',
-            '"2","unknown","1"\n',
             '"4","Some University - engineering","3"\n',
             '"8","Some University - other","3"\n',
-            '"5","rbb@some.edu","4"\n',
-            '"9","jim@some.edu","8"\n',
+            '"5","R B <rbb@some.edu>","4"\n',
+            '"9","J G <jim@some.edu>","8"\n',
         ],
         "group.csv": [
             '"Test Project 1-ff0e78e", "5"\n',
@@ -148,18 +146,77 @@ dataset = {
             '"Test Project 2-ff0e78e", "Test Project 2-ff0e78e", "nerc_openstack"\n',
         ],
     },
+    "2": {
+        "coldfront_data": [
+            {
+                "id": 1,
+                "project": {
+                    "id": 11,
+                    "title": "Test-Project-1",
+                    "pi": "rbb@some.edu",
+                    "description": "Experiment to run Autodesk Maya on GPU node",
+                    "field_of_science": "Visualization, Graphics, and Image Processing",
+                    "status": "New",
+                },
+                "description": None,
+                "resource": {"name": "NERC", "resource_type": "OpenStack"},
+                "status": "Active",
+                "attributes": {
+                    "Allocated Project ID": "2e68e6bf3ba84d96b77ff9cf8f1fffff",
+                    "Allocated Project Name": "Test Project 1-ff0e78e",
+                },
+            },
+        ],
+        "keycloak_data": [
+            {
+                "id": "7c6a4bea-37f4-46a3-8c52-4a93731fffff",
+                "createdTimestamp": 1672154253164,
+                "username": "rbb@some.edu",
+                "enabled": True,
+                "totp": False,
+                "emailVerified": True,
+                "firstName": "R",
+                "lastName": "B",
+                "email": "rbb@some.edu",
+                "attributes": {
+                    "cilogon_idp_name": ["Some University"],
+                    "mss_research_domain": ["engineering"],
+                },
+                "disableableCredentialTypes": [],
+                "requiredActions": [],
+                "notBefore": 0,
+                "access": {
+                    "manageGroupMembership": True,
+                    "view": True,
+                    "mapRoles": True,
+                    "impersonate": True,
+                    "manage": True,
+                },
+            },
+        ],
+        "hierarchy.csv": [
+            '"3","Some University",\n',
+            '"4","Some University - engineering","3"\n',
+            '"5","R B <rbb@some.edu>","4"\n',
+        ],
+        "group.csv": [
+            '"Test Project 1-ff0e78e", "5"\n',
+        ],
+        "names.csv": [
+            '"Test Project 1-ff0e78e", , "Test-Project-1 - Test Project 1-ff0e78e"\n',
+        ],
+        "pi2project.csv": [
+            '"Test Project 1-ff0e78e", "Test Project 1-ff0e78e", "nerc_openstack"\n',
+        ],
+    },
 }
 
 
 def test_000(mocker):
-    ds = dataset["0"]
-    mocker.patch(
-        "process_hierarchy.get_coldfront_data", return_value=ds["coldfront_data"]
-    )
-    mocker.patch(
-        "process_hierarchy.get_all_keycloak_data", return_value=ds["keycloak_data"]
-    )
     delete_hierarchy_db.delete_db()
+    ds = dataset["0"]
+    mocker.patch("process_hierarchy.get_coldfront_data", return_value=ds["coldfront_data"])
+    mocker.patch("process_hierarchy.get_all_keycloak_data", return_value=ds["keycloak_data"])
     process_hierarchy.main()
     output = {}
     for filename in ["hierarchy.csv", "group.csv", "pi2project.csv", "names.csv"]:
@@ -169,17 +226,27 @@ def test_000(mocker):
 
 
 def test_001(mocker):
-    ds = dataset["1"]
-    mocker.patch(
-        "process_hierarchy.get_coldfront_data", return_value=ds["coldfront_data"]
-    )
-    mocker.patch(
-        "process_hierarchy.get_all_keycloak_data", return_value=ds["keycloak_data"]
-    )
     delete_hierarchy_db.delete_db()
+    ds = dataset["1"]
+    mocker.patch("process_hierarchy.get_coldfront_data", return_value=ds["coldfront_data"])
+    mocker.patch("process_hierarchy.get_all_keycloak_data", return_value=ds["keycloak_data"])
     process_hierarchy.main()
     output = {}
     for filename in ["hierarchy.csv", "group.csv", "pi2project.csv", "names.csv"]:
         with open(filename, "r", encoding="utf-8") as file:
             output[filename] = file.readlines()
         assert output[filename] == ds[filename]
+
+
+def test_002(mocker):
+    delete_hierarchy_db.delete_db()
+    for ds_id in ["1", "2"]:
+        ds = dataset[ds_id]
+        mocker.patch("process_hierarchy.get_coldfront_data", return_value=ds["coldfront_data"])
+        mocker.patch("process_hierarchy.get_all_keycloak_data", return_value=ds["keycloak_data"])
+        process_hierarchy.main()
+        output = {}
+        for filename in ["hierarchy.csv", "group.csv", "pi2project.csv", "names.csv"]:
+            with open(filename, "r", encoding="utf-8") as file:
+                output[filename] = file.readlines()
+            assert output[filename] == ds[filename]
